96843b080bf5452b0db6bbef432268b4e3dbc36b
hailey123 <hailey.musselman@gmail.com>
2018-11-09 16:38:42 -0800

    made simple board, configured linting

---
+++ src/components/Board/Board.test.tsx
@@ -0,0 +1,6 @@
+describe('Board', () => {
+  it('should have dimensions 8x8', () => {
+    // TODO
+    expect(true).toBeTruthy();
+  });
+});
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import './Board.css';
+
+export interface IProps {
+  playAsBlack?: boolean;
+  lightSquareColor?: string;
+  darkSquareColor?: string;
+}
+
+const ranks = [1, 2, 3, 4, 5, 6, 7, 8];
+const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+class Board extends React.PureComponent<IProps> {
+  isDarkSquare(rankIndex: number, fileIndex: number): boolean {
+    console.log(rankIndex, fileIndex);
+
+    return !(fileIndex % 2) && !!(rankIndex % 2) || !!(fileIndex % 2) && !(rankIndex % 2);
+  }
+
+  render(): JSX.Element {
+    const {
+      playAsBlack = false,
+      darkSquareColor = 'darkslategrey',
+      lightSquareColor = 'bisque'
+    } = this.props;
+    let orderedRanks: number[];
+    let orderedFiles: string[];
+
+    if (playAsBlack) {
+      orderedRanks = ranks;
+      orderedFiles = files.reverse();
+    } else {
+      orderedRanks = ranks.reverse();
+      orderedFiles = files;
+    }
+
+    return (
+      <div className="board">
+        {
+          orderedRanks.map((rank, rankIndex) =>
+            <div key={rank} className="rank">
+              {orderedFiles.map((file, fileIndex) => <div
+                key={file}
+                className="square"
+                style={{
+                  backgroundColor: this.isDarkSquare(rankIndex, fileIndex)
+                    ? darkSquareColor
+                    : lightSquareColor,
+                }}>{file + rank}</div>)}
+            </div>,
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default Board;

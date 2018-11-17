2c65bab8fdbefb5a6e2366c4c9b61aad3c655659
hailey123 <hailey.musselman@gmail.com>
2018-11-17 11:43:58 -0800

    tested coordinatesEqual

--- src/lib/boardUtils.test.ts
+++ src/lib/boardUtils.test.ts
@@ -1,4 +1,5 @@
 import * as utils from './boardUtils';
+import { Coordinate } from 'src/types';
 
 describe('board utils', () => {
   const maxRandomInt = 1 - 1e-16;
@@ -22,12 +23,15 @@ describe('board utils', () => {
       mockRandom.mockRestore();
     });
   });
+
   describe('generateNextValue', () => {
     let values: number[];
+
     beforeAll(() => {
       values = [15, 3, 7, 8, 95, 33, -3, 24];
       Object.freeze(values);
     });
+
     it('returns the first possible value', () => {
       const mockRandom = jest.spyOn(Math, 'random').mockReturnValue(0);
 
@@ -41,6 +45,37 @@ describe('board utils', () => {
       expect(utils.generateNextValue(values)).toBe(values[values.length - 1]);
 
       mockRandom.mockRestore();
+    });
+  });
+
+  describe('coordinatesEqual', () => {
+    let coordinates: Coordinate;
+
+    beforeAll(() => {
+      coordinates = {
+        file: 'G',
+        rank: 8
+      };
+      Object.freeze(coordinates);
+    });
+
+    it('recognizes equal coordinates', () => {
+      expect(utils.coordinatesEqual(coordinates, {
+        file: 'G',
+        rank: 8
+      })).toBeTruthy();
+    });
+    it('recognizes unequal files', () => {
+      expect(utils.coordinatesEqual(coordinates, {
+        file: 'A',
+        rank: 8
+      })).toBeFalsy();
+    });
+    it('recognizes unequal ranks', () => {
+      expect(utils.coordinatesEqual(coordinates, {
+        file: 'G',
+        rank: 1
+      })).toBeFalsy();
     });
   });
 });

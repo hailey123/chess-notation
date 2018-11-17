30706806c1e0811a0a1c721daa7cf14b7d57f5ae
hailey123 <hailey.musselman@gmail.com>
2018-11-17 08:54:06 -0800

    refactored test

--- src/reducers/game.test.ts
+++ src/reducers/game.test.ts
@@ -1,36 +1,42 @@
 import game from './game';
 import { SHOW_NEXT_COORDS } from '../constants/actions';
-import { GameState } from 'src/types';
+import { GameState, Coordinate } from 'src/types';
 import * as util from '../lib/boardUtils';
 
 describe('game reducer', () => {
-  it('should return the initial state', () => {
-    const initialState: GameState = {
+  let initialState: GameState;
+  beforeAll(() => {
+    initialState = {
       inGameLoop: false,
       coordsPerRound: 8,
       showingTimingResult: false,
       currentCoords: { file: 'F', rank: 3 }
     };
+    Object.freeze(initialState);
+  });
+  it('should return the initial state', () => {
     expect(game(undefined, {} as any)).toEqual(initialState);
   });
   it('should handle SHOW_NEXT_COORDS', () => {
-    const stateBefore: GameState = {
-      inGameLoop: false,
-      coordsPerRound: 8,
-      showingTimingResult: false,
-      currentCoords: { file: 'F', rank: 3 }
+    const nextCoordinates: Coordinate = {
+      file: 'H',
+      rank: 6
     };
+    Object.freeze(nextCoordinates);
     const stateAfter: GameState = {
-      inGameLoop: false,
-      coordsPerRound: 8,
-      showingTimingResult: false,
-      currentCoords: { file: 'H', rank: 6 }
+      ...initialState,
+      currentCoords: nextCoordinates
     };
-    jest.spyOn(util, 'generateRandomCoords').mockReturnValue({
-      file: 'H', rank: 6
-    });
-    expect(game(stateBefore, {
+    const mockGenerateRandomCoordinates = jest.spyOn(util, 'generateRandomCoords').mockReturnValue(
+      nextCoordinates
+    );
+    expect(game(initialState, {
       type: SHOW_NEXT_COORDS
     })).toEqual(stateAfter);
+
+    // TODO: jest.restoreAllMocks in afterEach currently doesn't work.
+    // After ejecting, see if I can fix this.
+    // In the meantime, mocks must be individually restored.
+    mockGenerateRandomCoordinates.mockRestore();
   });
 });

cc1110914ed36b63bbf6a90a12caf73a8c310490
hailey123 <hailey.musselman@gmail.com>
2018-11-17 08:41:03 -0800

    test for SHOW_NEXT_COORDS

--- src/reducers/game.test.ts
+++ src/reducers/game.test.ts
@@ -1,12 +1,36 @@
 import game from './game';
+import { SHOW_NEXT_COORDS } from '../constants/actions';
+import { GameState } from 'src/types';
+import * as util from '../lib/boardUtils';
 
 describe('game reducer', () => {
   it('should return the initial state', () => {
-    expect(game(undefined, {} as any)).toEqual({
+    const initialState: GameState = {
       inGameLoop: false,
       coordsPerRound: 8,
       showingTimingResult: false,
       currentCoords: { file: 'F', rank: 3 }
+    };
+    expect(game(undefined, {} as any)).toEqual(initialState);
+  });
+  it('should handle SHOW_NEXT_COORDS', () => {
+    const stateBefore: GameState = {
+      inGameLoop: false,
+      coordsPerRound: 8,
+      showingTimingResult: false,
+      currentCoords: { file: 'F', rank: 3 }
+    };
+    const stateAfter: GameState = {
+      inGameLoop: false,
+      coordsPerRound: 8,
+      showingTimingResult: false,
+      currentCoords: { file: 'H', rank: 6 }
+    };
+    jest.spyOn(util, 'generateRandomCoords').mockReturnValue({
+      file: 'H', rank: 6
     });
+    expect(game(stateBefore, {
+      type: SHOW_NEXT_COORDS
+    })).toEqual(stateAfter);
   });
 });
--- src/reducers/game.ts
+++ src/reducers/game.ts
@@ -1,30 +1,7 @@
 import { Action } from '../actions';
-import { GameState, Coordinate } from '../types';
-import { files, ranks } from '../constants/models';
+import { GameState } from '../types';
 import { SHOW_NEXT_COORDS, HANDLE_SQUARE_CLICKED } from '../constants/actions';
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- */
-function getRandomInt(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function generateNextCoord<T>(possibleCoords: T[], currentCoord?: T): T {
-  const maxCoordIndex = possibleCoords.length - 1;
-  let nextCoord = possibleCoords[getRandomInt(0, maxCoordIndex)];
-  while (nextCoord === currentCoord) {
-    nextCoord = possibleCoords[getRandomInt(0, maxCoordIndex)];
-  }
-  return nextCoord;
-}
-
-function generateRandomCoords(lastCoordinates?: Coordinate): Coordinate {
-  const nextRank = generateNextCoord(ranks, lastCoordinates ? lastCoordinates.rank : undefined);
-  const nextFile = generateNextCoord(files, lastCoordinates ? lastCoordinates.file : undefined);
-
-  return { rank: nextRank, file: nextFile };
-}
+import { generateRandomCoords } from '../lib/boardUtils';
 
 export default function game(
   state: GameState = {

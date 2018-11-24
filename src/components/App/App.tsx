12a6209249b86a30b8bf02e1a2568e42075a861a
hailey123 <hailey.musselman@gmail.com>
2018-11-24 10:44:11 -0800

    colors and material app bar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,50 @@
 import * as React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Board from '../Board';
 import NotationInstruction from '../NotationInstruction';
 import StartButton from '../StartButton';
 
 import './App.css';
-// import logo from './logo.svg';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7'
+    },
+    secondary: {
+      main: '#1de9b6'
+    },
+  },
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          {/* <img src={logo} className='app-logo' alt='logo' /> */}
-          <h1 className="app-title">Board Coordinates Practice</h1>
-        </header>
-        <main>
-          <div className="left" />
-          <div className="mid">
-            <NotationInstruction />
-            <Board />
-          </div>
-          <div className="right">
-            <StartButton />
-          </div>
-        </main>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="app">
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Board Coordinates Practice
+          </Typography>
+            </Toolbar>
+          </AppBar>
+          <main>
+            <div className="left" />
+            <div className="mid">
+              <NotationInstruction />
+              <Board />
+            </div>
+            <div className="right">
+              <StartButton />
+            </div>
+          </main>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }

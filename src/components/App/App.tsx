a1f120ce4b6a8a1f43a1953d61625e1d401fe002
hailey123 <hailey.musselman@gmail.com>
2018-11-24 10:47:19 -0800

    extracted theme to its own file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,24 +2,14 @@ import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import Board from '../Board';
 import NotationInstruction from '../NotationInstruction';
 import StartButton from '../StartButton';
+import theme from 'src/theme';
 
 import './App.css';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#673ab7'
-    },
-    secondary: {
-      main: '#1de9b6'
-    },
-  },
-});
 
 class App extends React.Component {
   public render() {
@@ -27,7 +17,7 @@ class App extends React.Component {
       <MuiThemeProvider theme={theme}>
         <div className="app">
           <AppBar position="static">
-            <Toolbar>
+            <Toolbar variant="dense">
               <Typography variant="h6" color="inherit">
                 Board Coordinates Practice
           </Typography>
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7'
+    },
+    secondary: {
+      main: '#1de9b6'
+    },
+  },
+});

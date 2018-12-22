ed9bee8cca6330885b25b4eeb306a5aac9b470a6
hailey123 <hailey.musselman@gmail.com>
2018-12-22 14:27:21 -0800

    replaced mapDispatchToProps with action creator objects, tested BoardSquare container

--- src/components/BoardSquare/index.ts
+++ src/components/BoardSquare/index.ts
@@ -1,8 +1,7 @@
 import { connect } from 'react-redux';
 
 import BoardSquare from './BoardSquare';
-import { Dispatch } from 'react';
-import { Action, handleSquareClicked } from '../../actions';
+import { handleSquareClicked } from '../../actions';
 import { PropsPassedIn } from './props';
 import { StoreState } from '../../types';
 import { coordinatesEqual } from '../../lib/boardUtils';
@@ -12,10 +11,6 @@ export function mapStateToProps({ game }: StoreState, ownProps: PropsPassedIn) {
   return { isTarget: targetCoords && coordinatesEqual(targetCoords, ownProps.coordinate) };
 }
 
-export function mapDispatchToProps(dispatch: Dispatch<Action>) {
-  return {
-    handleClickAtCoordinate: (isTarget: boolean) => { dispatch(handleSquareClicked(isTarget)); }
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(BoardSquare);
+export default connect(mapStateToProps, {
+  handleClickAtCoordinate: handleSquareClicked
+})(BoardSquare);

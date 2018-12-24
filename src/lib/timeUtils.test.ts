f01a770cfbaf05d2e7edc5719b8d915fa7637907
hailey123 <hailey.musselman@gmail.com>
2018-12-24 08:18:24 -0700

    wrote test for minutesFromSeconds

--- src/lib/timeUtils.test.ts
+++ src/lib/timeUtils.test.ts
@@ -1,6 +1,13 @@
-import { secondsUnder60 } from './timeUtils';
+import { secondsUnder60, minutesFromSeconds } from './timeUtils';
 
 describe('time utils', () => {
+  describe('minutesFromSeconds', () => {
+    test('returns the correct value when total seconds >= 60', () => {
+      const totalSeconds = 70;
+      const minutesInSeconds = 1;
+      expect(minutesFromSeconds(totalSeconds)).toBe(minutesInSeconds);
+    });
+  });
   describe('secondsUnder60', () => {
     test('returns the correct value when total seconds >= 60', () => {
       const totalSeconds = 70;

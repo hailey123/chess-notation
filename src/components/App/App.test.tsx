e699fec0a16cb675ce95fbd7fa1f1f24b58069e3
hailey123 <hailey.musselman@gmail.com>
2018-12-25 13:17:01 -0700

    added come tests for App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
+describe('App', () => {
+  let wrapper: ShallowWrapper<any, {}, App>;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('has an AppBar header', () => {
+    expect(wrapper.children().contains('AppBar'));
+  });
+  it('has 3 panels', () => {
+    expect(wrapper.find('div.panel-before').length).toBe(1);
+    expect(wrapper.find('div.panel-main').length).toBe(1);
+    expect(wrapper.find('div.panel-after').length).toBe(1);
+  });
+  it('has a start button', () => {
+    expect(wrapper.children().contains('StartButton'));
+  });
+  it('has a round countdown', () => {
+    expect(wrapper.children().contains('RoundCountdown'));
+  });
+  it('has a counter', () => {
+    expect(wrapper.children().contains('Counter'));
+  });
 });

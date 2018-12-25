9e276a6fcf9aacff8e7e3e120565f1485ea1de72
hailey123 <hailey.musselman@gmail.com>
2018-12-24 19:53:01 -0700

    increased round length to 60s

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -4,4 +4,4 @@ export const Ranks: Rank[] = [1, 2, 3, 4, 5, 6, 7, 8];
 export const Files: File[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 export const RoundStartCountdownSeconds = 3;
-export const RoundLengthSeconds = 5;
+export const RoundLengthSeconds = 60;

a45bdbdcc99155d8181a8ca5e45f481ac98f469b
hailey123 <hailey.musselman@gmail.com>
2018-12-27 07:49:35 -0700

    made round timer red when expired

--- src/components/RoundCountdown/RoundCountdown.tsx
+++ src/components/RoundCountdown/RoundCountdown.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classnames from 'classnames';
 
 import { Props } from './props';
 import { clockTimeFromSeconds, clockTimeToString } from '../../lib/timeUtils';
@@ -7,9 +8,11 @@ import './RoundCountdown.css';
 
 class RoundCountdown extends React.PureComponent<Props> {
   render(): JSX.Element {
+    const { secondsRemaining } = this.props;
     const clockTime = clockTimeFromSeconds(this.props.secondsRemaining);
     const displayTime = clockTimeToString(clockTime);
-    return <div className="countdown-timer">
+    const classNames = classnames('countdown-timer', secondsRemaining === 0 ? 'finished' : '');
+    return <div className={classNames}>
       {displayTime}
     </div>;
   }

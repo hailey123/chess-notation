fee44032dfeb2c2e2fa1c7a27f6e4cda35021c14
hailey123 <hailey.musselman@gmail.com>
2018-12-29 14:33:49 -0700

    toggle board direction in reducer

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,10 +1,18 @@
 import { SettingsState } from '../types';
 import { Action } from '../actions';
 import { BaseSettingsState } from '../constants/models';
+import { TOGGLE_BOARD_DIRECTION } from '../constants/actions';
 
 export default function settings(
   state: SettingsState = BaseSettingsState,
-  _action: Action
+  action: Action
 ): SettingsState {
+  switch (action.type) {
+    case TOGGLE_BOARD_DIRECTION:
+      return {
+        ...state,
+        playAsBlack: !state.playAsBlack
+      };
+  }
   return state;
 }

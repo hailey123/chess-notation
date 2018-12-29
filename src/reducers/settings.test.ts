185f79dd4dccfe60059b5f673438ada513b37780
hailey123 <hailey.musselman@gmail.com>
2018-12-29 16:46:33 -0700

    tests for settings actions and reducer

--- src/reducers/settings.test.ts
+++ src/reducers/settings.test.ts
@@ -1,4 +1,8 @@
 import settings from './settings';
+import { SettingsState } from '../types';
+import { BaseSettingsState } from '../constants/models';
+import { ToggleBoardDirection } from '../actions';
+import { TOGGLE_BOARD_DIRECTION } from '../constants/actions';
 
 describe('settings reducer', () => {
   it('should return the initial state', () => {
@@ -7,5 +11,21 @@ describe('settings reducer', () => {
       lightSquareColor: '#eae1d7',
       darkSquareColor: '#353535'
     });
+  });
+  it('should handle TOGGLE_BOARD_DIRECTION', () => {
+    const stateBefore: SettingsState = {
+      ...BaseSettingsState,
+      playAsBlack: false
+    };
+    Object.freeze(stateBefore);
+    const stateAfter: SettingsState = {
+      ...BaseSettingsState,
+      playAsBlack: true
+    };
+    const action: ToggleBoardDirection = {
+      type: TOGGLE_BOARD_DIRECTION
+    };
+
+    expect(settings(stateBefore, action)).toEqual(stateAfter);
   });
 });
